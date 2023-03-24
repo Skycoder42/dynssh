1eea7875dfbe3fdc36798483ae15f5335055a47b
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-03-24 17:08:47 +0100

    create dart project

---
+++ bin/dynssh.dart
@@ -0,0 +1,5 @@
+import 'package:dynssh/dynssh.dart' as dynssh;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dynssh.calculate()}!');
+}
---
+++ test/dynssh_test.dart
@@ -0,0 +1,8 @@
+import 'package:dynssh/dynssh.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
